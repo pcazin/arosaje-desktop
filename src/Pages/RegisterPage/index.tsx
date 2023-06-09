0f7a52ae002b26823f718d5ce74266a886a9f8dc
pcazin <pablocazin.dev@gmail.Com>
2023-06-10 01:34:31 +0200

    mise a jour des formulaires connexion/inscription

--- src/Pages/RegisterPage/index.tsx
+++ src/Pages/RegisterPage/index.tsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
-import * as CONSTANTS from "../../constants";
+import { useNavigate, NavLink } from "react-router-dom";
 import authService from "../../services/AuthService";
 import "./styles.css";
 import React from "react";
 
-export default function LoginPage() {
+export default function RegisterPage() {
     const navigate = useNavigate();
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const result = await authService.login(username, password);
-
-        if (result.token) {
-            navigate("/");
-        } else {
-            alert("Les identifiants ne correspondent pas.");
-        }
+    const handlePasswordChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setPassword(event.target.value.slice(0, 15).toLowerCase());
     };
 
     const handleUsernameChange = (
         event: React.ChangeEvent<HTMLInputElement>
     ) => {
-        setUsername(event?.target.value.slice(0, 15).toLowerCase());
+        setUsername(event.target.value.slice(0, 15).toLowerCase());
     };
 
-    const handlePasswordChange = (
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        setPassword(event?.target.value.slice(0, 15).toLowerCase());
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // send register request
+        const result = await authService.register(username, password);
+
+        if (result.token) {
+            // navigate to home
+            navigate("/");
+        } else {
+            // show the error
+            alert("Erreur de création, essayer avec d'autres identifiants.");
+        }
     };
 
     return (
@@ -74,14 +76,14 @@ export default function LoginPage() {
                         autoComplete="off"
                     />
                 </div>
-                <NavLink to="/register" className="links">
-                    Créer un compte
+                <NavLink to="/login" className="links">
+                    Déjà utilisateur ? Se connecter
                 </NavLink>
                 <button
                     type="submit"
                     className="bg-green-700 text-white rounded-lg py-2 px-4 text-sm font-medium focus:outline-none focus:ring focus:ring-green-700"
                 >
-                    Connexion
+                    Inscription
                 </button>
             </form>
         </div>
